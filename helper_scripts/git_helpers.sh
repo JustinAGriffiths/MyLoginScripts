079c1d372ecd2a86f2c2bb032eb7fc7e75b1f46e
jgriffi6 <jgriffi6@gmail.com>
2017-01-12 12:17:57 -0600

    up

--- helper_scripts/git_helpers.sh
+++ helper_scripts/git_helpers.sh
@@ -4,7 +4,7 @@
 
 alias git_push='git push -u origin master'
 alias git_info='git remote show origin'
-alias git_pull='git pull'
+alias git_pull='git pull -a'
 alias git_up='git_pull'
 alias git_ci='git commit -a -m "up"'
 alias git_ls='git ls-tree -r'
