82d3282c71cc4bff250d123006962038a958c8f5
jgriffi6 <jgriffi6@gmail.com>
2017-01-13 15:30:08 -0600

    up

--- python/prompt.py
+++ python/prompt.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import os
 
 rows,columns=os.popen('stty size', 'r').read().split()
 
 import datetime
 
-#print (datetime.datetime.now().ctime()+'_'+os.getcwd()).ljust(int(columns),'_')
-print (datetime.datetime.now().ctime()+'_'+os.popen('pwd').read().split()[0]).ljust(int(columns),'_')
+print ((datetime.datetime.now().ctime()+'_'+os.popen('pwd').read().split()[0]).ljust(int(columns),'_'))
