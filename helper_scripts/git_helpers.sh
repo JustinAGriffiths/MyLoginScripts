88014fea1e9a1446af23c118e3376c348b23e95f
jgriffi6 <jgriffi6@gmail.com>
2017-01-13 11:49:28 -0600

    up

--- helper_scripts/git_helpers.sh
+++ helper_scripts/git_helpers.sh
@@ -3,8 +3,18 @@
 #http://git.or.cz/course/svn.html
 
 alias git_push='git push -u origin master'
-alias git_info='git remote show origin'
+#alias git_info='git remote show origin'
 alias git_pull='git pull -a'
 alias git_up='git_pull'
 alias git_ci='git commit -a -m "up"'
 alias git_ls='git ls-tree -r'
+
+
+git_info() {
+    cwd=`pwd`
+    if [[ $1 && -d $1 ]]; then
+	cd $1
+    fi
+    git remote show origin
+    cd $cwd
+}
